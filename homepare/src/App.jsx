1ac1bf4042b2536c5cec7c93971532b488976d9a
Kelsey Barham <kelseylbarham@gmail.com>
2024-01-10 13:28:54 -0500

    installed mantine library to use a component for a hamburger menu icon that hovers with an X. Need to format menu list next.

--- homepare/src/App.jsx
+++ homepare/src/App.jsx
@@ -4,12 +4,15 @@ import { Dashboard } from './dashboard'
 import { Register } from './register'
 import { Login } from './login'
 import { Route, Routes } from 'react-router-dom'
+import '@mantine/core/styles.css';
+import { MantineProvider } from '@mantine/core';
 
 function App() {
   
 
   return (
     <>
+    <MantineProvider>
     <Routes> 
       <Route
         path="/"
@@ -24,6 +27,7 @@ function App() {
         element={<Login />}
         />
     </Routes>
+    </MantineProvider>
   
     </>
   )
--- homepare/src/Menu.jsx
+++ homepare/src/Menu.jsx
@@ -1,10 +1,16 @@
 import { UserAccount } from "./UserAccount";
 import { Checklist } from "./checklist";
 import { ListingInput } from "./listingInput";
+import { useDisclosure } from '@mantine/hooks';
+import { Burger } from '@mantine/core';
+
 
 export function Menu() {
+    const [opened, { toggle }] = useDisclosure();
+
     return (
         <>
+        <Burger opened={opened} onClick={toggle} aria-label="Toggle navigation" />;
         <p>🍔 Menu:</p>
         <p>Add Listing</p>
         <ListingInput />
