647d2df909521f782df780e881c964bf76b6384a
Maria Angeles <maria.d.juri@gmail.com>
2024-01-09 20:12:18 -0500

    got my listings and my collections tabs working, added some placeholder house emojis to user collections page

--- homepare/src/UserCollections.jsx
+++ homepare/src/UserCollections.jsx
@@ -5,6 +5,10 @@ export function UserCollections() {
     return (
         <>
         <h1> My Collections </h1>
+        <p>🏠 🏠 🏠 🏠 🏠 🏠 🏠 🏠</p>
+        Collection 1
+        <p>🏠 🏠 🏠 🏠 🏠 🏠 🏠 🏠</p>
+        Collection 2
         <CollectionDetail />
         <ComparisonTable />
         </>
