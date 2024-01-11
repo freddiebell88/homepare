13f7f618efabf570b5e9b771481b58d25ba915d4
freddiebell88 <frederikcwbell@gmail.com>
2024-01-11 17:12:22 -0500

    wrote conditional for rendering details, not working properly yet

--- homepare/src/UserListings.jsx
+++ homepare/src/UserListings.jsx
@@ -2,29 +2,37 @@ import { ListingDetails } from './ListingDetails'
 import { DetailsCard } from './detailsCard'
 import { ListingInput, Preview } from './listingInput'
 import homeData from './data/homes.json'
+import { useState } from 'react';
 
 export function UserListings( ) {
-    // const handleSelectListing = () = {
-    //     return (
-    //         <DetailsCard />
-    //     )
+    
+    const [clicked, setClicked] = useState('false')
+    
     const thumbWidth = "100px";
     const thumbHeight = "100px";
 
     const handleOpenThumbnail = () => {
-        <DetailsCard />
+        console.log('thumbnail clicked')
+        return (
+            setClicked('true')
+        )
     }
+
 
     return (
         <>
         <h1> My Listings </h1>
 {/* listing thumbnail could be a component wrapped in a context provider? */}
-
+        {!clicked ? 
         <div onClick={handleOpenThumbnail} className='listing-thumbnail'>
         <img src={homeData.value[0].Media[0].Thumbnail} width={thumbWidth} height={thumbHeight}/>
         <p>{homeData.value[0].UnparsedAddress}</p>
         </div>
-        
+        : 
+        <div>
+        <DetailsCard />
+        </div>
+        }
         <div className='listing-thumbnail'>
         <img src={homeData.value[1].Media[2].Thumbnail} width={thumbWidth} height={thumbHeight}/>
         <p>{homeData.value[1].UnparsedAddress}</p>
