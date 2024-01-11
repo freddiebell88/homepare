10362b344aa9ca9da45241c5485870596aa74ee4
freddiebell88 <frederikcwbell@gmail.com>
2024-01-11 10:45:54 -0500

    showing dummy thumbnails on my listings

--- homepare/src/UserListings.jsx
+++ homepare/src/UserListings.jsx
@@ -1,13 +1,15 @@
 import { ListingDetails } from './ListingDetails'
 import { DetailsCard } from './detailsCard'
 import { ListingInput, Preview } from './listingInput'
+import homeData from './data/homes.json'
 
 export function UserListings( ) {
     // const handleSelectListing = () = {
     //     return (
     //         <DetailsCard />
     //     )
-    
+    const thumbWidth = "100px";
+    const thumbHeight = "100px";
 
     return (
         <>
@@ -15,20 +17,29 @@ export function UserListings( ) {
         <div> 
             {/* <p>🏠</p>
             <p >Address 1</p> */}
-            <Preview />
+            {/* <Preview /> */}
         </div>
-        <div>
-        <p>🏠</p>
-            Address 2 </div>
-        <div>
-        <p>🏠</p>
-            Address 3 </div>
-        <div>
-        <p>🏠</p>
-            Address 4 </div>
-            
-        {/* <ListingDetails /> */}
-        {/* <ListingInput /> */}
+
+        <div className='listing-thumbnail'>
+        <img src={homeData.value[0].Media[0].Thumbnail} width={thumbWidth} height={thumbHeight}/>
+        <p>{homeData.value[0].UnparsedAddress}</p>
+        </div>
+        
+        <div className='listing-thumbnail'>
+        <img src={homeData.value[1].Media[2].Thumbnail} width={thumbWidth} height={thumbHeight}/>
+        <p>{homeData.value[1].UnparsedAddress}</p>
+        </div>
+
+        <div className='listing-thumbnail'>
+        <img src={homeData.value[2].Media[5].Thumbnail} width={thumbWidth} height={thumbHeight}/>
+        <p>{homeData.value[2].UnparsedAddress}</p>
+        </div>
+
+        <div className='listing-thumbnail'>
+        <img src={homeData.value[2].Media[7].Thumbnail} width={thumbWidth} height={thumbHeight}/>
+        <p>{homeData.value[2].UnparsedAddress}</p>
+        </div>
+        
         </>
     )
 }
