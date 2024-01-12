e55d550dcdf9b2ffeef65f274fa2ac4d0768f8d9
freddiebell88 <frederikcwbell@gmail.com>
2024-01-12 12:15:03 -0500

    adds mantine modal to pop up listing details on my listings page next will add to search and collections

--- homepare/src/UserListings.jsx
+++ homepare/src/UserListings.jsx
@@ -3,47 +3,45 @@ import { DetailsCard } from './detailsCard'
 import { ListingInput, Preview } from './listingInput'
 import homeData from './data/homes.json'
 import { useState } from 'react';
+import { useDisclosure } from '@mantine/hooks';
+import { Modal, Button } from '@mantine/core';
 
 export function UserListings( ) {
     
-    const [clicked, setClicked] = useState('false')
     
     const thumbWidth = "100px";
     const thumbHeight = "100px";
 
-    const handleOpenThumbnail = () => {
-        console.log('thumbnail clicked')
-        return (
-            setClicked('true')
-        )
-    }
 
+    const [opened, { open, close }] = useDisclosure(false);
 
     return (
         <>
         <h1> My Listings </h1>
 {/* listing thumbnail could be a component wrapped in a context provider? */}
-        {!clicked ? 
-        <div onClick={handleOpenThumbnail} className='listing-thumbnail'>
+        <Modal opened={opened} onClose={close} centered>
+            <DetailsCard />
+        </Modal>
+        
+        <div  onClick={open} className='listing-thumbnail'>
         <img src={homeData.value[0].Media[0].Thumbnail} width={thumbWidth} height={thumbHeight}/>
         <p>{homeData.value[0].UnparsedAddress}</p>
         </div>
-        : 
-        <div>
-        <DetailsCard />
-        </div>
-        }
-        <div className='listing-thumbnail'>
+        
+        
+        
+        
+        <div onClick={open} className='listing-thumbnail'>
         <img src={homeData.value[1].Media[2].Thumbnail} width={thumbWidth} height={thumbHeight}/>
         <p>{homeData.value[1].UnparsedAddress}</p>
         </div>
 
-        <div className='listing-thumbnail'>
+        <div onClick={open} className='listing-thumbnail'>
         <img src={homeData.value[2].Media[5].Thumbnail} width={thumbWidth} height={thumbHeight}/>
         <p>{homeData.value[2].UnparsedAddress}</p>
         </div>
 
-        <div className='listing-thumbnail'>
+        <div onClick={open} className='listing-thumbnail'>
         <img src={homeData.value[2].Media[7].Thumbnail} width={thumbWidth} height={thumbHeight}/>
         <p>{homeData.value[2].UnparsedAddress}</p>
         </div>
@@ -51,3 +49,27 @@ export function UserListings( ) {
         </>
     )
 }
+
+// export function UserListings({homeData}) {
+//     return (
+//         <div>
+//             {homeData.map((listing, index) => (
+//                 <ListingThumbnail 
+//                     key={index} 
+//                     thumbnail={listing.value[0].Media[0].Thumbnail}
+//                     streetAddress={listing.value[0].UnparsedAddress}
+//                     />
+//             ))}
+//         </div>
+//     )
+// }
+
+
+// export function ListingThumbnail({thumbnail, streetAddress}) {
+//     return (
+//         <div>
+//             <img src={thumbnail} />
+//             <p>{streetAddress}</p>
+//         </div>
+//     )
+// }
