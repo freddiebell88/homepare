01272d86fb18181ac429057dfb3d9ceec9731829
freddiebell88 <frederikcwbell@gmail.com>
2024-01-12 16:07:56 -0500

    input state updates onchange typing into search bar

--- homepare/src/listingInput.jsx
+++ homepare/src/listingInput.jsx
@@ -42,28 +42,19 @@ export function Preview( {thumbnail, streetAddress} ) {
 }
 
 const SearchBar = () => {
-    const [searchInput, setSearchInput] = useState('');
+    const [input, setInput] = useState('');
 
-    // const streetAddress = {homeData.value[0].UnparsedAddress}
+    
 
-    const handleChange = (e) => {
-        e.preventDefault();
-        setSearchInput(e.target.value);
-    };
-
-    if (searchInput.length > 0) {
-        homeData.filter((listing) => {
-            return listing.UnparsedAddress.match(searchInput);
-        })
-    }
+    
 
     return (
         <form>
             <input
                 type="search"
                 placeholder="Input Listing"
-                onChange={handleChange}
-                value={searchInput}
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
             />
             <button>Search</button>
 
@@ -71,41 +62,4 @@ const SearchBar = () => {
     )
 }
 
-export default SearchBar
-// const SearchBarFilter = () => {
-//     const [searchTerm, setSearchTerm] = useState('');
-//     const [filteredData, setFilteredData] = useState(homeData);
 
-//     const handleInputChange = (event) => {
-//         const { value } = event.target;
-//         setSearchTerm(value);
-//         filterData(value);
-//     };
-
-//     const filterData = (searchTerm) => {
-//         const filteredData = homeData.filter((item) => 
-//         item.name.toUpperCase().includes(searchTerm.toUpperCase())
-//         );
-//         setFilteredData(filteredData);
-//     };
-
-    
-//     return (
-//         <div>
-//             <input 
-//                 type="text"
-//                 placeholder="Input Listing Address..."
-//                 value={searchTerm}
-//                 onChange={handleInputChange}
-//             />
-
-//             <div>
-//                 {filteredData.map((item) => (
-//                     <li key={item.value}>{item.UnparsedAddress}</li>
-//                 ))}
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default SearchBarFilter
