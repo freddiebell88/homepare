017427d0de92e2ef219dbe712cbcaac5c95a8785
freddiebell88 <frederikcwbell@gmail.com>
2024-01-12 17:21:12 -0500

    framework for registration page. all fields there but not able to submit yet

--- homepare/src/register.jsx
+++ homepare/src/register.jsx
@@ -1,7 +1,53 @@
 import { Questionnaire } from "./questionnaire";
+import { TextInput, Button, Group, Box } from '@mantine/core';
+import { useForm } from '@mantine/form';
 
 export function Register() {
+    const form = useForm({
+        initialValues: {
+        username: '',
+        password: '',
+        email: '',
+        first_name: '',
+        last_name: '',
+        },
+
+        validate: {
+
+        },
+    })
+
     return (
-        <Questionnaire />
+        <>
+        <form onSubmit={form.onSubmit((values) => console.log(values))}>
+            <TextInput 
+            label="Username"
+            placeholder="Create Username"
+            {...form.getInputProps('username')}
+            />
+            <TextInput 
+            label="Password"
+            placeholder="Create Password"
+            {...form.getInputProps('password')}
+            />
+            <TextInput 
+            label="Email"
+            placeholder="Enter your email address"
+            {...form.getInputProps('email')}
+            />
+            <TextInput 
+            label="First Name"
+            placeholder="Enter your First Name"
+            {...form.getInputProps('first_name')}
+            />
+            <TextInput 
+            label="Last Name"
+            placeholder="create last name"
+            {...form.getInputProps('last_name')}
+            />
+        </form>
+        <Button type="submit">Register</Button>
+        {/* <Questionnaire /> */}
+        </>
     )
 }
