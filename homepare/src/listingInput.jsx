075f9b6c8c01028e302f7f3a8291f7b292c456b0
freddiebell88 <frederikcwbell@gmail.com>
2024-01-17 10:36:35 -0500

    state updates with address from api request

--- homepare/src/listingInput.jsx
+++ homepare/src/listingInput.jsx
@@ -71,62 +71,70 @@ const SearchBar = () => {
     // },[])
 
     useEffect(() => {
-        axios.get('https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/basicprofile?address=1311%20Carolina%20Loop%20Hillsborough%20NC', {
+        axios.get('https://api.gateway.attomdata.com/propertyapi/v1.0.0/property/basicprofile?address=1601%20East%20Barden%20Rd%20Charlotte%20NC', {
             headers: {
                 Accept: 'application/json',
                 apikey: '2b1e86b638620bf2404521e6e9e1b19e',
             }
-        }).then((response) =>{console.log(response.data)})
-    })
+        })
+        .then((response) =>{
+            console.log(response.data);
+            setListingList(response.data.property[0].address.oneLine);
+            })
+        }, [])
+
 
     return (
+        <>
         <form>
             <input
-                type="search"
+                type="text"
                 placeholder="Input Listing"
                 onChange={(e)=>setInput(e.target.value)}
                 value={input}
             />
             <button>Look Up</button>
-            <h2>Results:</h2>
-            {listingList.filter((listing) => {
-                if(input===""){
-                    return listing
-                 } else if (listing.address.toLowerCase().includes(input.toLowerCase())){
-                    return listing
-                    }
-                    })
-                .map((listing) => {
-                    return (
-                    <>
-                    {/* <Modal opened={opened} onClose={close} centered>
-                    <DetailsCard 
-                    address={listing.address}
-                    thumbImage={listing.images[0].Thumbnail}
-                    />
-                    </Modal> */}
-                    <Preview 
-                        key={listing._id}
-                        address={listing.address}
-                        previewImage={listing.images[0].Thumbnail}
-                        listingID={listing._id}
-                        squareFootage={listing.living_area}
-                        bathrooms={listing.bathrooms}
-                        bedrooms={listing.bedrooms}
-                        propertyType={listing.property_type}
-                        hoa={listing.hoa}
-                        garage={listing.garage}
-                        price={listing.price}
-                        />
-                    {/* <div key={listing._id} onClick={open}>
-                        <img src={listing.images[0].Thumbnail}/>
-                        <h3>{listing.address}</h3>
-                    </div> */}
-                    </>
-                    )
-            })}
-
         </form>
+            <h2>Results:</h2>
+
+        </>
+            // {listingList.filter((listing) => {
+            //     if(input===""){
+            //         return listing
+            //      } else if (listing.address.toLowerCase().includes(input.toLowerCase())){
+            //         return listing
+            //         }
+            //         })
+            //     .map((listing) => {
+            //         return (
+            //         <>
+            //         {/* <Modal opened={opened} onClose={close} centered>
+            //         <DetailsCard 
+            //         address={listing.address}
+            //         thumbImage={listing.images[0].Thumbnail}
+            //         />
+            //         </Modal> */}
+            //         <Preview 
+            //             key={listing._id}
+            //             address={listing.address}
+            //             previewImage={listing.images[0].Thumbnail}
+            //             listingID={listing._id}
+            //             squareFootage={listing.living_area}
+            //             bathrooms={listing.bathrooms}
+            //             bedrooms={listing.bedrooms}
+            //             propertyType={listing.property_type}
+            //             hoa={listing.hoa}
+            //             garage={listing.garage}
+            //             price={listing.price}
+            //             />
+            //         {/* <div key={listing._id} onClick={open}>
+            //             <img src={listing.images[0].Thumbnail}/>
+            //             <h3>{listing.address}</h3>
+            //         </div> */}
+            //         </>
+            //         )
+            // })}
+
     )
 }
 
