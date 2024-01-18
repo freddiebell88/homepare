e57292824bd86449135312892c1e3037160ebb88
freddiebell88 <frederikcwbell@gmail.com>
2024-01-18 13:59:05 -0500

    my listings tab shows listings from db - needs to be associated with user

--- homepare/src/UserListings.jsx
+++ homepare/src/UserListings.jsx
@@ -2,50 +2,64 @@ import { ListingDetails } from './ListingDetails'
 import { DetailsCard } from './detailsCard'
 import { ListingInput } from './listingInput'
 import homeData from './data/homes.json'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useDisclosure } from '@mantine/hooks';
 import { Modal, Button } from '@mantine/core';
+import axios from 'axios';
 
-export function UserListings( {myListings} ) {
-    
+export function UserListings() {
     
     const thumbWidth = "100px";
     const thumbHeight = "100px";
-
-
+    const [myListings, setMyListings] = useState([])
     const [opened, { open, close }] = useDisclosure(false);
+    const placeholderImage = "homepare/src/data/pexels-kelly-2950003.jpg"
+
+    const usePlaceHolder = (e) => {
+        e.target.src = placeholderImage
+    }
+
+    useEffect(() => {
+    axios.get("https://homepare-backend.onrender.com/homes", {
+        headers: {
+            authorization: "x-access-token eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6Im5ld3VzZXI5IiwiaWF0IjoxNzA1NTk1NDY5LCJleHAiOjE3MDU2ODE4Njl9.S1kPErLtGajmty_NF5sOUEle56onmCjpZ9svk-K1eOc"
+            }
+    }).then((res) => {
+    console.log(res.data.homes);
+    setMyListings(res.data.homes)})
+    }, [])
 
     return (
         <>
         <h1> My Listings </h1>
-{/* listing thumbnail could be a component wrapped in a context provider? */}
+        {myListings.map((mylisting) => {
+            return (
+        <>
         <Modal opened={opened} onClose={close} centered>
             <DetailsCard 
-            myListings={myListings} />
+            // myListings={myListings}
+            address={mylisting.address}
+            previewImage={mylisting.images[0]}
+            squareFootage={mylisting.living_area}
+            halfBathrooms={mylisting.half_bath}
+            bathrooms={mylisting.full_bath}
+            bedrooms={mylisting.bedrooms}
+            propertyType={mylisting.property_type}
+            hoa={mylisting.hoa}
+            garage={mylisting.garage}
+            price={mylisting.price}
+            listingId={mylisting._id}
+            />
         </Modal>
         
-        <div  onClick={open} className='listing-thumbnail'>
-        <img src={homeData.value[0].Media[0].Thumbnail} width={thumbWidth} height={thumbHeight}/>
-        <p>{homeData.value[0].UnparsedAddress}</p>
+        <div  key={mylisting._id} onClick={open} className='listing-thumbnail'>
+        <img src={mylisting.images[0]} width={thumbWidth} height={thumbHeight}/>
+        <p>{mylisting.address}</p>
         </div>
+        </>
+            )
+        })}
         
-        
-        
-        
-        <div onClick={open} className='listing-thumbnail'>
-        <img src={homeData.value[1].Media[2].Thumbnail} width={thumbWidth} height={thumbHeight}/>
-        <p>{homeData.value[1].UnparsedAddress}</p>
-        </div>
-
-        <div onClick={open} className='listing-thumbnail'>
-        <img src={homeData.value[2].Media[5].Thumbnail} width={thumbWidth} height={thumbHeight}/>
-        <p>{homeData.value[2].UnparsedAddress}</p>
-        </div>
-
-        <div onClick={open} className='listing-thumbnail'>
-        <img src={homeData.value[2].Media[7].Thumbnail} width={thumbWidth} height={thumbHeight}/>
-        <p>{homeData.value[2].UnparsedAddress}</p>
-        </div>
         
         </>
     )
