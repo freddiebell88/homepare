716bdd2486c0c0e7e99657649768ae7aabcc5f80
Kelsey Barham <kelseylbarham@gmail.com>
2024-01-20 11:14:27 -0500

    /checklist is now has ternary operators for yes/no

--- homepare/src/checklist.jsx
+++ homepare/src/checklist.jsx
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
+import { Button, Container } from "@mantine/core"
 
 export function Checklist ({token}) {
     const [preferences, setPreferences] = useState({
@@ -25,14 +26,18 @@ export function Checklist ({token}) {
     console.log('this is checklist')
     return(
         <>
+        <Container
+        size="30rem"
+        >
         <h1>Checklist</h1>
         <h2>You are looking for:</h2>
-        <h3>{`${preferences.bedrooms} Bedroom`}</h3>
-        <h3>{`${preferences.bathrooms} Bathroom home`}</h3>
-        <h3>{`Yard: ${preferences.yard}`}</h3>
-        <h3>{`Garage: ${preferences.garage}`}</h3>
-        <h3>{`HOA: ${preferences.hoa}`}</h3>
-        {/* yard, garage, and HOA can disappear if user selects 'not sure' */}
+        <h3>{`${preferences.bedrooms} Bedrooms`}</h3>
+        <h3>{`${preferences.bathrooms} Bathrooms`}</h3>
+        <h3>{`Yard: ${preferences.yard === true ? 'Yes' : 'No'}`}</h3>
+        <h3>{`Garage: ${preferences.garage === true ? 'Yes' : 'No'}`}</h3>
+        <h3>{`HOA: ${preferences.hoa === true ? 'Yes' : 'No'}`}</h3>
+        </Container>
         </>
     )
 }
+
