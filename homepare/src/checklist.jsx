4f952d599019a3af86ab8b5e2111b440d39dabd5
Kelsey Barham <kelseylbarham@gmail.com>
2024-01-20 10:57:46 -0500

    /checklist is now pulling user preference data from the questionnaire

--- homepare/src/checklist.jsx
+++ homepare/src/checklist.jsx
@@ -1,16 +1,37 @@
-// the checklist is generated from our user's questionnaire results
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-export function Checklist () {
+export function Checklist ({token}) {
+    const [preferences, setPreferences] = useState({
+        bathrooms : 0,
+        bedrooms : 0,
+        garage : false ,
+        hoa : false,
+        yard : false
+    });
+
+    useEffect(() => {
+    axios.get('https://homepare-backend.onrender.com/user-preference',{
+        headers: {
+          authorization: `x-access-token ${token}`
+        }
+      }).then((res) => {
+        setPreferences(res.data)
+        console.log(res.data)
+      })}, [token])
+
+
+
     console.log('this is checklist')
     return(
         <>
         <h1>Checklist</h1>
-        <h2>You are looking for a:</h2>
-        <h3>__ Bedroom</h3>
-        <h3>__ Bathroom home</h3>
-        <h3>with/without a yard</h3>
-        <h3>with/without a garage</h3>
-        <h3>with/without a HOA</h3>
+        <h2>You are looking for:</h2>
+        <h3>{`${preferences.bedrooms} Bedroom`}</h3>
+        <h3>{`${preferences.bathrooms} Bathroom home`}</h3>
+        <h3>{`Yard: ${preferences.yard}`}</h3>
+        <h3>{`Garage: ${preferences.garage}`}</h3>
+        <h3>{`HOA: ${preferences.hoa}`}</h3>
         {/* yard, garage, and HOA can disappear if user selects 'not sure' */}
         </>
     )
