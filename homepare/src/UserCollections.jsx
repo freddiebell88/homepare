723e7411ec0fef2d883281db146835649ee6bd36
freddiebell88 <frederikcwbell@gmail.com>
2024-01-22 15:07:39 -0500

    shows all collection names on collection tab

--- homepare/src/UserCollections.jsx
+++ homepare/src/UserCollections.jsx
@@ -15,120 +15,33 @@ export function UserCollections( {token}) {
   const thumbHeight = "100px";
 
   const [opened, { open, close }] = useDisclosure(false);
+  const [myCollections, setMyCollections] = useState([]);
  
-
-  // const handleNewCollectionClick = () => {
-  //   console.log("new collection button clicked");
-  //   <Modal opened={opened} onClose={close} centered>
-  //     <NewCollection token={token} />
-  //   </Modal>;
-  // };
+  useEffect(() => {
+    axios.get("https://homepare-backend.onrender.com/collections", {
+        headers: {
+            authorization: `x-access-token ${token}`
+            }
+    }).then((res) => {
+    console.log(res.data.search);
+    setMyCollections(res.data.search)})
+    }, [ token ])
 
   return (
     <>
-      <Modal opened={opened} onClose={close} centered>
+      {/* <Modal opened={opened} onClose={close} centered>
         <DetailsCard />
-      </Modal>
-      <div className="collections-wrapper-in-user-collections">
-        Collection 1 Name
-        <div className="userCollection">
-          {/* The name of the collection comes directly from the user */}
-          <div onClick={open} className="listing-thumbnail-in-user-collections">
-            <img
-              src={homeData.value[0].Media[0].Thumbnail}
-              width={thumbWidth}
-              height={thumbHeight}
-            />
-            <p className="thumbnail-text-in-user-collections">
-              {homeData.value[0].UnparsedAddress}
-            </p>
-          </div>
+      </Modal> */}
 
-          <div onClick={open} className="listing-thumbnail-in-user-collections">
-            <img
-              src={homeData.value[1].Media[2].Thumbnail}
-              width={thumbWidth}
-              height={thumbHeight}
-            />
-            <p className="thumbnail-text-in-user-collections">
-              {homeData.value[1].UnparsedAddress}
-            </p>
+      {myCollections.map((collection) => {
+        return (
+          <>
+          <div key={collection._id} className="collections-wrapper-in-user-collections">
+            {collection.search_name}
           </div>
-
-          <div onClick={open} className="listing-thumbnail-in-user-collections">
-            <img
-              src={homeData.value[2].Media[5].Thumbnail}
-              width={thumbWidth}
-              height={thumbHeight}
-            />
-            <p className="thumbnail-text-in-user-collections">
-              {homeData.value[2].UnparsedAddress}
-            </p>
-          </div>
-
-          <div onClick={open} className="listing-thumbnail-in-user-collections">
-            <img
-              src={homeData.value[2].Media[7].Thumbnail}
-              width={thumbWidth}
-              height={thumbHeight}
-            />
-            <p className="thumbnail-text-in-user-collections">
-              {homeData.value[2].UnparsedAddress}
-            </p>
-          </div>
-
-          <div onClick={open} className="listing-thumbnail-in-user-collections">
-            <img
-              src={homeData.value[0].Media[0].Thumbnail}
-              width={thumbWidth}
-              height={thumbHeight}
-            />
-            <p className="thumbnail-text-in-user-collections">
-              {homeData.value[0].UnparsedAddress}
-            </p>
-          </div>
-
-          <div onClick={open} className="listing-thumbnail-in-user-collections">
-            <img
-              src={homeData.value[1].Media[2].Thumbnail}
-              width={thumbWidth}
-              height={thumbHeight}
-            />
-            <p className="thumbnail-text-in-user-collections">
-              {homeData.value[1].UnparsedAddress}
-            </p>
-          </div>
-
-          <div onClick={open} className="listing-thumbnail-in-user-collections">
-            <img
-              src={homeData.value[2].Media[5].Thumbnail}
-              width={thumbWidth}
-              height={thumbHeight}
-            />
-            <p className="thumbnail-text-in-user-collections">
-              {homeData.value[2].UnparsedAddress}
-            </p>
-          </div>
-
-          <div onClick={open} className="listing-thumbnail-in-user-collections">
-            <img
-              src={homeData.value[2].Media[7].Thumbnail}
-              width={thumbWidth}
-              height={thumbHeight}
-            />
-            <p className="thumbnail-text-in-user-collections">
-              {homeData.value[2].UnparsedAddress}
-            </p>
-          </div>
-        </div>
-        <Link to="/CollectionDetail"><p className="see-more-in-user-collections">See More</p></Link>
-        {/* See more should pull up collection details */}
-      <Divider size="xs" />
-      </div>
-
-      <div className="collections-wrapper-in-user-collections"></div>
-      {/* <CollectionDetail />
-        <ComparisonTable /> */}
+          </>
+        )
+      })}
 
       <NewCollection token={token} />
     </>
