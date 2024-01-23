c570344ecf28e1954499ce2c3406e727793800be
Maria Angeles <maria.d.juri@gmail.com>
2024-01-22 21:30:28 -0500

    fixed yes and no text display in chart

--- homepare/src/comparisonTable.jsx
+++ homepare/src/comparisonTable.jsx
@@ -1,88 +1,149 @@
 import dbhomes from "./data/homesfromDB.json";
-import { Table, ColorSwatch, Group, Tooltip } from '@mantine/core'
+import { Table, ColorSwatch, Group, Tooltip } from "@mantine/core";
 import axios from "axios";
 import { useEffect, useState } from "react";
 
-
 // useState for array of selected listings and map through those to build table?
 
-export function ComparisonTable ( {homeData, token} ) {
+export function ComparisonTable({ homeData, token }) {
   const [preferences, setPreferences] = useState({
-    bathrooms : 0,
-    bedrooms : 0,
-    garage : false ,
-    hoa : false,
-    yard : false
-});
+    bathrooms: 0,
+    bedrooms: 0,
+    garage: false,
+    hoa: false,
+    yard: false,
+  });
 
-useEffect(() => {
-axios.get('https://homepare-backend.onrender.com/user-preference',{
-    headers: {
-      authorization: `x-access-token ${token}`
-    }
-  }).then((res) => {
-    setPreferences(res.data)
-    console.log(res.data)
-  })}, [token])
-  
-// if {listing.bedrooms === preference.bedrooms ? className="table-test-class" : className="table-test-class-pink"}
-//if listing.bedrooms has the same value as preference.bedrooms the classname is table-test-class else table-test-class-pink
+  useEffect(() => {
+    axios
+      .get("https://homepare-backend.onrender.com/user-preference", {
+        headers: {
+          authorization: `x-access-token ${token}`,
+        },
+      })
+      .then((res) => {
+        setPreferences(res.data);
+        console.log(res.data);
+      });
+  }, [token]);
 
-    const rows = homeData.map((listing) => (
-        <Table.Tr key={listing.id}>
-            <Table.Td>{listing.address}</Table.Td>
-            <Table.Td>{listing.price}</Table.Td>
-            <Table.Td>{listing.living_area}</Table.Td>
-            <Table.Td className={listing.bedrooms === preferences.bedrooms ? "match-in-comparison-table" : "not-a-match-in-comparision-table"}>{listing.bedrooms}</Table.Td>
-            <Table.Td className={listing.bathrooms === preferences.bathrooms ? "match-in-comparison-table" : "not-a-match-in-comparision-table"}>{listing.bathrooms}</Table.Td>
-            <Table.Td className={listing.hoa === preferences.hoa ? "match-in-comparison-table" : "not-a-match-in-comparision-table"}>{listing.hoa}</Table.Td>
-            <Table.Td className={listing.yard === preferences.yard ? "match-in-comparison-table" : "not-a-match-in-comparision-table"}>{listing.yard}</Table.Td>
-            <Table.Td className={listing.garage === preferences.garage ? "match-in-comparison-table" : "not-a-match-in-comparision-table"}>{listing.garage}</Table.Td>
-        </Table.Tr>
-    ))
+  // if {listing.bedrooms === preference.bedrooms ? className="table-test-class" : className="table-test-class-pink"}
+  //if listing.bedrooms has the same value as preference.bedrooms the classname is table-test-class else table-test-class-pink
 
-    return (
-      <>
-      
-        <Table.ScrollContainer maxWidth={500}>
-            <Table striped highlightOnHover>
-            <Table.Thead>
-                <Table.Tr>
-                <Table.Th> 
-                  <Group><Tooltip
-                  label="Matches your Checklist">
-                    <ColorSwatch color="var(--mantine-color-green-light-color)">✔️</ColorSwatch></Tooltip>
-                    
-                    <Tooltip label="Does not match your Checklist"><ColorSwatch color="var(--mantine-color-red-8)">
-                    X
-                    </ColorSwatch></Tooltip>
-                  </Group>
-        </Table.Th>
-                <Table.Th>Price</Table.Th>
-                <Table.Th>SQ Footage</Table.Th>
-                <Table.Th>Bedrooms</Table.Th>
-                <Table.Th>Bathrooms</Table.Th>
-                <Table.Th>HOA</Table.Th>
-                <Table.Th>Yard</Table.Th>
-                <Table.Th>Garage</Table.Th>
-                </Table.Tr>
-            </Table.Thead>
-            <Table.Thead>
+  const rows = homeData.map((listing) => (
+    <Table.Tr key={listing.id}>
+      <Table.Td>{listing.address}</Table.Td>
+      <Table.Td>{listing.price}</Table.Td>
+      <Table.Td>{listing.living_area}</Table.Td>
+      <Table.Td
+        className={
+          listing.bedrooms === preferences.bedrooms
+            ? "match-in-comparison-table"
+            : "not-a-match-in-comparision-table"
+        }
+      >
+        {listing.bedrooms}
+      </Table.Td>
+      <Table.Td
+        className={
+          listing.bathrooms === preferences.bathrooms
+            ? "match-in-comparison-table"
+            : "not-a-match-in-comparision-table"
+        }
+      >
+        {listing.bathrooms}
+      </Table.Td>
+      <Table.Td
+        className={
+          listing.hoa === preferences.hoa
+            ? "match-in-comparison-table"
+            : "not-a-match-in-comparision-table"
+        }
+      >
+        {listing.hoa === true ? "Yes" : "No"}
+      </Table.Td>
+      <Table.Td
+        className={
+          listing.yard === preferences.yard
+            ? "match-in-comparison-table"
+            : "not-a-match-in-comparision-table"
+        }
+      >
+        {listing.yard === true ? "Yes" : "No"}
+      </Table.Td>
+      <Table.Td
+        className={
+          listing.garage === preferences.garage
+            ? "match-in-comparison-table"
+            : "not-a-match-in-comparision-table"
+        }
+      >
+        {listing.garage === true ? "Yes" : "No"}
+      </Table.Td>
+    </Table.Tr>
+  ));
+
+  return (
+    <>
+      <Table.ScrollContainer maxWidth={500}>
+        <Table striped highlightOnHover>
+          <Table.Thead>
             <Table.Tr>
-            <Table.Th className="checklist-row-in-comparison-table">My Checklist</Table.Th>
-                <Table.Th className="checklist-row-in-comparison-table">Price</Table.Th>
-                <Table.Th className="checklist-row-in-comparison-table">SQ Footage</Table.Th>
-                <Table.Th className="checklist-row-in-comparison-table">{preferences.bedrooms}</Table.Th>
-                <Table.Th className="checklist-row-in-comparison-table">{preferences.bathrooms}</Table.Th>
-                <Table.Th className="checklist-row-in-comparison-table">{preferences.hoa === true ? 'Yes' : 'No'}</Table.Th>
-                <Table.Th className="checklist-row-in-comparison-table">{preferences.yard === true ? 'Yes' : 'No'}</Table.Th>
-                <Table.Th className="checklist-row-in-comparison-table">{preferences.garage === true ? 'Yes' : 'No'}</Table.Th>
+              <Table.Th>
+                <Group>
+                  <Tooltip label="Matches your Checklist">
+                    <ColorSwatch color="var(--mantine-color-green-light-color)">
+                      ✔️
+                    </ColorSwatch>
+                  </Tooltip>
+
+                  <Tooltip label="Does not match your Checklist">
+                    <ColorSwatch color="var(--mantine-color-red-8)">
+                      X
+                    </ColorSwatch>
+                  </Tooltip>
+                </Group>
+              </Table.Th>
+              <Table.Th>Price</Table.Th>
+              <Table.Th>SQ Footage</Table.Th>
+              <Table.Th>Bedrooms</Table.Th>
+              <Table.Th>Bathrooms</Table.Th>
+              <Table.Th>HOA</Table.Th>
+              <Table.Th>Yard</Table.Th>
+              <Table.Th>Garage</Table.Th>
             </Table.Tr>
-            </Table.Thead>
-            <Table.Tbody>{rows}</Table.Tbody>
+          </Table.Thead>
+          <Table.Thead>
+            <Table.Tr>
+              <Table.Th className="checklist-row-in-comparison-table">
+                My Checklist
+              </Table.Th>
+              <Table.Th className="checklist-row-in-comparison-table">
+                Price
+              </Table.Th>
+              <Table.Th className="checklist-row-in-comparison-table">
+                SQ Footage
+              </Table.Th>
+              <Table.Th className="checklist-row-in-comparison-table">
+                {preferences.bedrooms}
+              </Table.Th>
+              <Table.Th className="checklist-row-in-comparison-table">
+                {preferences.bathrooms}
+              </Table.Th>
+              <Table.Th className="checklist-row-in-comparison-table">
+                {preferences.hoa === true ? "Yes" : "No"}
+              </Table.Th>
+              <Table.Th className="checklist-row-in-comparison-table">
+                {preferences.yard === true ? "Yes" : "No"}
+              </Table.Th>
+              <Table.Th className="checklist-row-in-comparison-table">
+                {preferences.garage === true ? "Yes" : "No"}
+              </Table.Th>
+            </Table.Tr>
+          </Table.Thead>
+          <Table.Tbody>{rows}</Table.Tbody>
         </Table>
-        </Table.ScrollContainer>
-        
-        </>
-    )
+      </Table.ScrollContainer>
+    </>
+  );
 }
