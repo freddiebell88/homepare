288dcf086db52a2c8b1ae19a1bf4f886e28f2b0b
freddiebell88 <frederikcwbell@gmail.com>
2024-02-01 11:50:48 -0500

    add to collection component in new file

--- homepare/src/detailsCard.jsx
+++ homepare/src/detailsCard.jsx
@@ -2,17 +2,8 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { IconHomeStar } from '@tabler/icons-react';
-import {
-  Card,
-  SimpleGrid,
-  Text,
-  Badge,
-  Button,
-  Group,
-  Image,
-  Textarea,
-} from "@mantine/core";
-import { IconQuestionMark } from "@tabler/icons-react";
+import { AddToCollection } from "./add-to-collection";
+import { Card, SimpleGrid, Text, Badge, Button, Group,Image, Textarea } from "@mantine/core";
 
 export function DetailsCard({
   token,
@@ -59,10 +50,6 @@ export function DetailsCard({
      });
   }, [token]);
 
-  //if previewimage wasnt passed in or error set a default value
-  // if (!previewImage) {
-  //   previewImage = 
-  // }
 
   const handleAddListingClick = () => {
     console.log("add listing button");
@@ -233,97 +220,5 @@ const getCompareIcon = (a,b) => {
   else return "❌";
 }
 
-export function  AddToCollection({ close, token, listingId, updateCollection }) {
-  const [myCollections, setMyCollections] = useState([]);
-  const [selectedCollection, setSelectedCollection] = useState("");
-  const [errorMessage, setErrorMessage] = useState("");
-  const [form, setForm] = useState({
-    search_name: "",
-  });
 
-
-    useEffect(() => {
-      axios
-        .get("https://homepare-backend.onrender.com/collections", {
-          headers: {
-            authorization: `x-access-token ${token}`,
-          },
-        })
-        .then((res) => {
-          setMyCollections(res.data.search);
-          console.log(`collections data ${res.data.search}`);
-          console.log(myCollections);
-        }).catch((err) => {
-          return setErrorMessage(err.response.data.message)
-       });
-    }, []);
-
-    if (myCollections.length != 0 ){
-
-      if (myCollections[0].search_name === "My List") {
-        myCollections[0].search_name = "My Homes"
-      }}
-
-    const handleCollectionChange = (e) => {
-      setForm({
-        ...form,
-        _id: e.target.value,
-        houseID: listingId,
-      });
-    };
-
-    const handleSubmit = (e) => {
-      e.preventDefault();
-      const selectedCollection = myCollections.find(
-        (collection) => collection._id === form._id
-      );
-      console.log(selectedCollection);
-      selectedCollection.houseID.push(listingId);
-      axios
-        .put(
-          `https://homepare-backend.onrender.com/collections/${form._id}`,
-          { ...selectedCollection },
-          {
-            headers: {
-              authorization: `x-access-token ${token}`,
-            },
-          }
-        )
-        .then(() => (axios.get(`https://homepare-backend.onrender.com/home/${listingId}`,
-        {
-          headers: {
-            authorization: `x-access-token ${token}`,
-          },
-        })))
-        .then((res) => updateCollection(res.data))
-
-        // .then(close())
-        .catch((err) => {
-          return setErrorMessage(err.response.data.message)
-       });
-       
-    };
-
-    return (
-      <>
-      { errorMessage && <Text c="red" >{errorMessage}</Text>}
-        <form method="post" onSubmit={handleSubmit}>
-          <label>
-            <select
-              value={selectedCollection}
-              onChange={handleCollectionChange}
-            >
-              <option>Add To Collection</option>
-              {myCollections.map((collection) => (
-                <option key={collection._id} value={collection._id}>
-                  {collection.search_name}
-                </option>
-              ))}
-            </select>
-          </label>
-          <Button type="submit" size="xs" color="gray">Add</Button>
-        </form>
-      </>
-    );
-  }
 
